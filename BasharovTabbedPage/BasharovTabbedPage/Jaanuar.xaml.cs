591b4a5f7236196f745442d70dfc043575502034
opilane <opilane@HP-CZC6297B8C>
2020-10-07 12:29:32 +0300

    Add project files.

---
+++ BasharovTabbedPage/BasharovTabbedPage/Jaanuar.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BasharovTabbedPage
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Jaanuar : ContentPage
+    {
+        public Jaanuar()
+        {
+            InitializeComponent();
+        }
+
+        private async void off_Clicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Alert", "You have been alerted", "OK");
+        }
+    }
+}
